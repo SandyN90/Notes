d6e33def36ddfddc41e0ba5270eca8a752a50e48
sandy <sandeepkumarnavik5@gmail.com>
2022-05-29 01:36:17 +0530

    Added header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import logo from './img/logo512.png';
+import logo1 from './img/logo192.png';
 
 export default function Header() {
   return (
     <>
-    <h1>
-        This is header
-    </h1>
+    <div className='flex'>
+        <img alt="img" src={logo} className="h-10 w-10 ml-2 mt-2"/>
+        <div className='flex items-center h-10 w-10 ml-2 mt-2'>
+            <img alt='img' src={logo1} className="mx-3"/>
+            <h1>Notes</h1>
+        </div>
+    </div>
     </>
   )
 }
