20aef4084401f06612b88ba1e33029cb15a53abc
sandy <sandeepkumarnavik5@gmail.com>
2022-05-29 16:15:01 +0530

    Added searchbar sidebar and mainContent components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import logo from './img/logo512.png';
 import logo1 from './img/notes.png';
+import SearchBar from './SearchBar';
 
 export default function Header() {
   return (
     <>
-    <div className='flex bg-indigo-100'>
+    <div className='flex justify-between bg-indigo-100'>
+        <div className='flex'>
         <img alt="img" src={logo} className="h-10 w-10 ml-2 mt-2 my-2"/>
         <div className='flex items-center h-10 w-10 ml-2 mt-2 my-2'>
             <img alt='img' src={logo1} className="mx-3"/>
             <h1>Notes</h1>
+        </div>
+        </div>
+        <div className='flex justify-between'>
+        <div className=''>
+        <SearchBar/>
+        </div>
+        <button className='content-end mx-6'>Sign In</button>
         </div>
     </div>
     </>
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SearchBar() {
+    return (
+        <>
+        <input className="bg-gray-300 w-1/2 h-12 my-1 ml-64 rounded-2xl" placeholder="       Search"/>
+        </>
+    )
+}
