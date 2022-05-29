6148d7f87ddd4da2a3001fb06936ebeed81692e3
sandy <sandeepkumarnavik5@gmail.com>
2022-05-29 09:28:37 +0530

    Added notes logo to the header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import logo from './img/logo512.png';
-import logo1 from './img/logo192.png';
+import logo1 from './img/notes.png';
 
 export default function Header() {
   return (
     <>
-    <div className='flex'>
-        <img alt="img" src={logo} className="h-10 w-10 ml-2 mt-2"/>
-        <div className='flex items-center h-10 w-10 ml-2 mt-2'>
+    <div className='flex bg-indigo-100'>
+        <img alt="img" src={logo} className="h-10 w-10 ml-2 mt-2 my-2"/>
+        <div className='flex items-center h-10 w-10 ml-2 mt-2 my-2'>
             <img alt='img' src={logo1} className="mx-3"/>
             <h1>Notes</h1>
         </div>
